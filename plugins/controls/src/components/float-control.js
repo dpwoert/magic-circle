aa20e1abab3e6124bfcfa071429e0c6d010126ff
Davey van der Woert <davey@dpwoert.com>
2019-01-16 21:44:17 +0000

    biDirectional float controls

--- plugins/controls/src/components/float-control.js
+++ plugins/controls/src/components/float-control.js
@@ -44,28 +44,38 @@ const Slider = styled.input`
 const Progress = styled.div`
   position: absolute;
   top: 0;
-  left: 0;
-  width: ${props => props.progress}%;
+  left: ${props => props.left};
+  width: ${props => props.width}%;
   height: 100%;
   background: ${props => new Color(props.theme.accent).alpha(0.2).toCSS()};
   cursor: grab;
 `;
 
+const mapLinear = (x, a1, a2, b1, b2) =>
+  b1 + ((x - a1) * (b2 - b1)) / (a2 - a1);
+
 const FloatControl = props => {
   const { value, options, updateControl } = props;
   const { range, stepSize } = options;
+  const biDirectional = Math.abs(range[0]) === Math.abs(range[1]);
+
   const progress =
     ((value - parseInt(range[0], 10)) * 100) /
     (parseInt(range[1], 10) - parseInt(range[0], 10));
   const extent = Math.abs(parseInt(range[1], 10) - parseInt(range[0], 10));
   const step = stepSize === 0 ? extent / 100 : stepSize;
 
+  const x1 = mapLinear(0, range[0], range[1], 0, 100);
+  const x2 = mapLinear(value, range[0], range[1], 0, 100);
+  const width = biDirectional ? Math.abs(x1 - x2) : progress;
+  const left = biDirectional ? Math.min(x1, x2) : 0;
+
   return (
     <Row>
       <Label>{props.label}</Label>
       <Center>
         <InputContainer>
-          <Progress progress={progress} />
+          <Progress left={left} width={width} />
           <Slider
             value={value}
             onChange={evt => {
