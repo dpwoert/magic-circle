b8c5e7bde44ec645ec7a57ed442aa3e246e3e031
Davey van der Woert <davey@dpwoert.com>
2019-07-20 21:41:05 +0100

    fixes references to color plugin

--- core/ui/src/default-settings.js
+++ core/ui/src/default-settings.js
@@ -2,30 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Layers from '@magic-circle/layers';
-// import PlayControls from '@magic-circle/play-controls';
-// import Controls from '@magic-circle/controls';
-// import Seed from '@magic-circle/seed';
-// import PageInfo from '@magic-circle/page-information';
-// import Screenshots from '@magic-circle/screenshots';
-// import Performance from '@magic-circle/performance';
-// import Touchbar from '@magic-circle/touchbar';
-// import Midi from '@magic-circle/midi';
-// import Fullscreen from '@magic-circle/fullscreen';
+import PlayControls from '@magic-circle/play-controls';
+import Controls from '@magic-circle/controls';
+import Seed from '@magic-circle/seed';
+import PageInfo from '@magic-circle/page-information';
+import Screenshots from '@magic-circle/screenshots';
+import Performance from '@magic-circle/performance';
+import Touchbar from '@magic-circle/touchbar';
+import Midi from '@magic-circle/midi';
+import Fullscreen from '@magic-circle/fullscreen';
 
 import Layout from './components/layout';
 
 const settings = {
   plugins: [
-    // PageInfo,
-    // Seed,
+    PageInfo,
+    Seed,
     Layers,
-    // PlayControls,
-    // // Controls,
-    // Screenshots,
-    // Performance,
-    // Fullscreen,
-    // Touchbar,
-    // Midi,
+    PlayControls,
+    Controls,
+    Screenshots,
+    Performance,
+    Fullscreen,
+    Touchbar,
+    Midi,
   ],
   theme: {
     // accent: 'rgb(102, 255, 153)',
