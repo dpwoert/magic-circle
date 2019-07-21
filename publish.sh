c14a9682c1174cf3b9c8f2635ccd720d7a32ff4c
Davey van der Woert <davey@dpwoert.com>
2019-07-21 15:42:36 +0100

    improves publish script

--- publish.sh
+++ publish.sh
@@ -2,17 +2,35 @@
 
 # stop when one of these commands fail
 set -e
+set -o pipefail
+
+read -p "This will create a new release and reinstall everything in production mode. Do you wish to continue? (y/n) " yn
+
+case $yn in
+    [Yy]* ) echo "\nseting up a developing environment";;
+    [Nn]* ) exit;;
+    * )
+      echo "Please answer yes or no."
+      exit;
+    ;;
+esac
+
+echo "cleaning repo and (re)installing dependencies"
+npm run clean
+npm install
+
+echo "building packages"
+npm run build:ui:prod
+npm run build:client:prod
+npm run package:prod
+
+# ensuring project is fully linted
+npm run lint
 
 # update version
 lerna version
 
-# reinstall all dependencies
-npm run clean
-npm install
-
-# rebuild all projects
-npm run build:prod
-npm run package:prod
+# git tag?
 
 # publish
 if [[ "$1" == "--confirm" ]]; then
