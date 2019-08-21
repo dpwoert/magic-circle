eb88a5085296e2d8dcb245476b184349bbb16d0a
Davey van der Woert <davey@dpwoert.com>
2019-08-21 20:36:28 +0100

    improves publish script, no longer runs with git changes

--- publish.sh
+++ publish.sh
@@ -6,6 +6,13 @@ set -o pipefail
 
 read -p "This will create a new release and reinstall everything in production mode. Do you wish to continue? (y/n) " yn
 
+if [[ `git status --porcelain` ]]; then
+  echo ""
+  echo "Git changes detected, make sure you've commited all your work before"
+  echo ""
+  exit 1
+fi
+
 case $yn in
     [Yy]* ) echo "\nseting up a developing environment";;
     [Nn]* ) exit;;
