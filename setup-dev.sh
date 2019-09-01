0de5834cbcf3059e48a2c11e9405c0728afc7b71
Davey van der Woert <davey@dpwoert.com>
2019-09-02 00:22:07 +0100

    fix for installing on CI

--- setup-dev.sh
+++ setup-dev.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-read -p "This will clean this repo first and reinstall all depedencies. Do you wish to continue? (y/n) " yn
+if [[ "$*" == "--yes" ]]
+then
+    echo "seting up a developing environment [CI mode]"
+else
+  read -p "This will clean this repo first and reinstall all depedencies. Do you wish to continue? (y/n) " yn
 
-case $yn in
-    [Yy]* ) echo "\nseting up a developing environment";;
-    [Nn]* ) exit;;
-    * )
-      echo "Please answer yes or no."
-      exit;
-    ;;
-esac
+  case $yn in
+      [Yy]* ) echo "\nseting up a developing environment";;
+      [Nn]* ) exit;;
+      * )
+        echo "Please answer yes or no."
+        exit;
+      ;;
+  esac
+fi
 
 echo "cleaning repo and (re)installing dependencies"
 npm run clean
