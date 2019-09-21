2e38ca8cd840a22d4def33938962c5b1b2eb13fc
Davey van der Woert <davey@dpwoert.com>
2019-09-22 00:00:44 +0100

    automatised index script for icons

--- core/ui/src/icons/index.js
+++ core/ui/src/icons/index.js
@@ -1,71 +1,78 @@
 /* eslint-disable no-unused-vars */
 import { CameraRoll } from './camera-roll.svg';
-import { Trashbin } from './trashbin.svg';
+import { Debug } from './debug.svg';
+import { Fullscreen } from './fullscreen.svg';
 import { HardReload } from './hard-reload.svg';
 import { Images } from './images.svg';
+import { Keyboard } from './keyboard.svg';
 import { Layers } from './layers.svg';
 import { Pause } from './pause.svg';
+import { Performance } from './performance.svg';
+import { Performance2 } from './performance2.svg';
 import { Play } from './play.svg';
+import { Play2 } from './play2.svg';
 import { Reload } from './reload.svg';
 import { Rewind } from './rewind.svg';
 import { Screenshot } from './screenshot.svg';
-import { Performance } from './performance.svg';
-import { Performance2 } from './performance2.svg';
-import { Debug } from './debug.svg';
-import { Fullscreen } from './fullscreen.svg';
-import { Videogame } from './videogame.svg';
-import { Keyboard } from './keyboard.svg';
+import { Trashbin } from './trashbin.svg';
 import { Usb } from './usb.svg';
+import { Videogame } from './videogame.svg';
 
 import cameraRoll from './camera-roll.png';
-import trashbin from './trashbin.png';
+import debug from './debug.png';
+import fullscreen from './fullscreen.png';
 import hardReload from './hard-reload.png';
 import images from './images.png';
+import keyboard from './keyboard.png';
 import layers from './layers.png';
 import pause from './pause.png';
+import performance from './performance.png';
+import performance2 from './performance2.png';
 import play from './play.png';
+import play2 from './play2.png';
 import reload from './reload.png';
 import rewind from './rewind.png';
 import screenshot from './screenshot.png';
-import performance from './performance.png';
-import performance2 from './performance2.png';
-import debug from './debug.png';
-import fullscreen from './fullscreen.png';
-import videogame from './videogame.png';
-import keyboard from './keyboard.png';
+import trashbin from './trashbin.png';
 import usb from './usb.png';
+import videogame from './videogame.png';
 
 CameraRoll.png = cameraRoll;
-Trashbin.png = trashbin;
+Debug.png = debug;
+Fullscreen.png = fullscreen;
 HardReload.png = hardReload;
 Images.png = images;
+Keyboard.png = keyboard;
 Layers.png = layers;
 Pause.png = pause;
+Performance.png = performance;
+Performance2.png = performance2;
 Play.png = play;
+Play2.png = play2;
 Reload.png = reload;
 Rewind.png = rewind;
 Screenshot.png = screenshot;
-Performance.png = performance;
-Performance2.png = performance2;
-Debug.png = debug;
-Fullscreen.png = fullscreen;
+Trashbin.png = trashbin;
+Usb.png = usb;
+Videogame.png = videogame;
 
 export {
   CameraRoll,
-  Trashbin,
+  Debug,
+  Fullscreen,
   HardReload,
   Images,
+  Keyboard,
   Layers,
   Pause,
+  Performance,
+  Performance2,
   Play,
+  Play2,
   Reload,
   Rewind,
   Screenshot,
-  Performance,
-  Performance2,
-  Debug,
-  Fullscreen,
+  Trashbin,
   Usb,
-  Keyboard,
   Videogame,
 };
