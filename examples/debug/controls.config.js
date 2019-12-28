96341f204715211df4ca2bc373e4961c2acc5c06
Davey van der Woert <davey@dpwoert.com>
2019-12-28 18:40:25 +0100

    loads midi at debug example

--- examples/debug/controls.config.js
+++ examples/debug/controls.config.js
@@ -1,8 +1,9 @@
 import Debug from '@magic-circle/debug';
 import Recordings from '@magic-circle/recordings';
+import Midi from '@magic-circle/midi';
 
 module.exports = {
-  plugins: defaultPlugins => [...defaultPlugins, Debug, Recordings],
+  plugins: defaultPlugins => [...defaultPlugins, Debug, Recordings, Midi],
   recordings: {
     enableFFMPEG: true,
   },
