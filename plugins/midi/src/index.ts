49c6bf900d52ba30b266c6c7859bb0b3197a1411
Davey van der Woert <davey@dpwoert.com>
2022-07-07 11:48:20 +0100

    removes redundant dependencies and security updates

---
+++ plugins/midi/src/index.ts
@@ -0,0 +1,13 @@
+import { Plugin, App, LayoutHook } from '@magic-circle/schema';
+
+export default class Midi implements Plugin {
+  ipc: App['ipc'];
+  client: App;
+
+  name = 'midi';
+
+  async setup(client: App) {
+    this.ipc = client.ipc;
+    this.client = client;
+  }
+}
