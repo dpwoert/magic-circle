ea1e15c61bade9c95b002e69d36152055e903ed5
Davey van der Woert <davey@dpwoert.com>
2022-08-22 22:57:18 +0100

    improved support for image

--- core/client/src/controls/image.ts
+++ core/client/src/controls/image.ts
@@ -4,11 +4,33 @@ export default class ImageControl extends Control<string> {
   type = 'image';
 
   constructor(
-    reference: Control<string>['reference'] | HTMLImageElement,
+    reference: Control<string>['reference'] | HTMLImageElement | ImageBitmap,
     key?: string
   ) {
-    const safeKey = !key && 'src' in reference ? 'src' : key;
-    super(reference, safeKey);
+    let safeKey = key;
+    let referenceSafe = reference;
+
+    if (!key && 'src' in reference) {
+      safeKey = 'src';
+    } else if (
+      !key &&
+      'close' in reference &&
+      reference.toString() === '[object ImageBitmap]'
+    ) {
+      const canvas = document.createElement('canvas');
+      const context = canvas.getContext('2d');
+      canvas.width = reference.width;
+      canvas.height = reference.height;
+
+      // @ts-expect-error
+      context.drawImage(reference, 0, 0, reference.width, reference.height);
+      const url = canvas.toDataURL('image/png');
+
+      referenceSafe = { value: url };
+      safeKey = 'value';
+    }
+
+    super(referenceSafe, safeKey);
 
     if (!key) {
       this.label('Image');
