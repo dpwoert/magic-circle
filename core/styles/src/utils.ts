f0fd6c15f551df72375c707fe90afc7b53f60492
Davey van der Woert <davey@dpwoert.com>
2022-09-26 23:03:08 +0100

    fixes small bug when rounding

--- core/styles/src/utils.ts
+++ core/styles/src/utils.ts
@@ -1,4 +1,6 @@
 export const formatNumber = (n: number, maxDigits = 5) => {
   const digits = n.toString().split('.')[1];
-  return digits && digits.length >= 5 ? n.toFixed(maxDigits) : n.toString();
+  return digits && digits.length >= maxDigits
+    ? n.toFixed(maxDigits)
+    : n.toString();
 };
