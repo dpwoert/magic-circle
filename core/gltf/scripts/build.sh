f15f24386e9027dfbbd3f7175e296f71ecd7e709
Davey van der Woert <davey@dpwoert.com>
2024-01-15 21:25:57 +0000

    trying to fix build script

--- core/gltf/scripts/build.sh
+++ core/gltf/scripts/build.sh
@@ -4,12 +4,9 @@ rm -rf magic-circle
 # Build Magic Circle
 npx magic build
 
-# ensure we have an example dir
-mkdir magic-circle/viewer
-
 # open folder for inner frame
 cd ../../examples/three-gltf-upload
 
 # build
 npm run build:prod
-cp -R dist ../../core/online/magic-circle/viewer/
+cp -R dist ../../core/online/magic-circle/viewer
