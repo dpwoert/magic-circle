4d3e106283621e2117f2baeb8a4bcd8e681e9487
Davey van der Woert <davey@dpwoert.com>
2024-01-27 12:19:31 +0000

    adds dedupe for gltf example

--- examples/three-gltf/vite.config.ts
+++ examples/three-gltf/vite.config.ts
@@ -6,5 +6,8 @@ export default defineConfig({
     port: 4000,
     strictPort: true,
   },
+  resolve: {
+    dedupe: ['three'],
+  },
   base: process.env.NODE_ENV === 'production' ? '/examples/three-gltf/' : '/',
 });
